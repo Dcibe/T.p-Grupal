c3220c5f3bfd02146b43ff6cb3d75c18670da4a1
Dcibe <54721704+Dcibe@users.noreply.github.com>
2019-08-30 15:18:09 -0300

    Add files via upload

---
+++ TP. Grupal.py
@@ -0,0 +1,19 @@
+class IdLocal:
+    def __init__(self,local,nro):
+        self.nro=nro
+        self.local=local
+    def __repr__(self):
+        return "[Local: %s, Sucursal Nº: %i]" % (self.local, self.nro)
+    
+l1=IdLocal("Santa Teresita",1)
+print (l1)
+
+def alta ():
+    
+    return
+def baja ():
+    
+    return
+def mostrar ():
+    
+    return
