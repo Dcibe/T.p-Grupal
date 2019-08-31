d814a67574a67883808ac15809cfc1dd772d806e
Dcibe <54721704+Dcibe@users.noreply.github.com>
2019-08-30 23:19:14 -0300

    Update TP. Grupal.py

--- TP. Grupal.py
+++ TP. Grupal.py
@@ -1,3 +1,4 @@
+##ARCHIVO
 class IdLocal:
     def __init__(self,local,nro):
         self.nro=nro
@@ -5,12 +6,27 @@ class IdLocal:
     def __repr__(self):
         return "[%s,%i]" % (self.local, self.nro)
     
-
-listaLocal = [IdLocal("",0)] * 10
-
+##LISTA
+listaLocal = [IdLocal("",0)] * 10   
 for f in range(0,6):
     listaLocal[f]= IdLocal("Sucursal", f * 2)
+    
+##Oredena la lista
+def ordenar ():
+    for i in range(0,9):
+        for j in range(0,9):
+            if listaLocal[j].local.lower() > listaLocal[j + 1].local.lower():
+                if listaLocal[j+1].local != "":
+                    aux= listaLocal[j]
+                    listaLocal[j]= listaLocal[j + 1]
+                    listaLocal[j + 1]= aux
+            elif listaLocal[j].local == "":
+                    aux= listaLocal[j]
+                    listaLocal[j]= listaLocal[j + 1]
+                    listaLocal[j + 1]= aux
+    return
 
+##Alta a una nueva sucursal
 def alta ():
     for i in range (0,10):
         if listaLocal[i].local== "":
@@ -18,6 +34,7 @@ def alta ():
             while True:
                 try:
                     nro= int(input("Ingrese Nro Sucursal:"))
+                    ordenar()
                     break
                 except ValueError:
                     print("El dato ingresado no es un numero")
@@ -25,10 +42,24 @@ def alta ():
             return
 
 f= alta()
-
+##Baja una sucursal
 def baja ():
+    while True:
+        try:
+            sucursal= int(input("Baja sucursal Nº: "))
+            break
+        except ValueError:
+            print("El dato ingresado no es un numero")
+    for i in range (0,10):
+        if listaLocal[i].nro==sucursal:
+            listaLocal[i]=IdLocal("",0)
+            ordenar()
+            return
     
-    return
+    return print( "Numero de sucursal ingresado es invalido" )
+b= baja()
+
+##Interfaz Grafca
 def mostrar ():
     
     return
